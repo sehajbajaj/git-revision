98d26a6c3b016f516499b218ff6f43715c325068
sehajbajaj <sehajbajaj2001@gmail.com>
2024-07-06 16:28:22 +0530

    Applying stashed changes and adding settings

--- hello.py
+++ hello.py
@@ -1,18 +1,10 @@
 print('Hello World')
 Name = input('What is your name?')
 print(f'Hello, {Name}.')
-
 print('Welcome to the Hello World App.')
 print('Master branch changes')
-<<<<<<< HEAD
-
-
-print('Welcome to the Hello World App.')
-
-
 print('Goodbye.')
 print('See you soon.')
-
 print('Dashboard')
 print('Welcome to Dashboar')
 
